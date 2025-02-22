331e37c43e03dca2a18b059b077808f514d1c167
haoxt04 <k42.dkhao@gmail.com>
2025-02-22 10:07:24 +0700

    trang thông tin chi tiết sản phẩm DetailProduct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DetailProductComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [
+    // HomeComponent,
+    DetailProductComponent
+    // OrderComponent
+  ]
 })
 export class AppModule { }
