11bc1ca373a0b07a25825983cd09d3e1f6a382c7
haoxt04 <k42.dkhao@gmail.com>
2025-02-25 15:45:31 +0700

    model order, order detail và order status

---
+++ shopapp-backend/shopapp/src/main/java/com/project/shopapp/model/Order.java
@@ -0,0 +1,62 @@
+package com.project.shopapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
+@Data
+public class Order extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "fullname", length = 100)
+    private String fullName;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false, length = 100)
+    private String phoneNumber;
+
+    @Column(name = "address", length = 100)
+    private String address;
+
+    @Column(name = "note")
+    private String note;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "total_money")
+    private String totalMoney;
+
+    @Column(name = "shipping_method")
+    private String shippingMethod;
+
+    @Column(name = "shipping_address")
+    private String shippingAddress;
+
+    @Column(name = "shipping_date")
+    private Date shippingDate;
+
+    @Column(name = "tracking_number")
+    private String trackingNumber;
+
+    @Column(name = "payment_method")
+    private String paymentMethod;
+
+    @Column(name = "active")
+    private boolean active;     // thuộc về admin
+}
