09c55362d70b620bb28474fbc961451a38f84a20
haoxt04 <k42.dkhao@gmail.com>
2025-03-05 22:58:20 +0700

    chức năng chọn Role form đăng nhập

--- shopapp-angular/src/app/component/login/login.component.ts
+++ shopapp-angular/src/app/component/login/login.component.ts
@@ -5,6 +5,8 @@ import { UserService } from '../../service/user.service';
 import { NgForm } from '@angular/forms';
 import { LoginResponse } from '../../response/user/login.response';
 import { TokenService } from '../../service/token.service';
+import { RoleService } from '../../service/role.service';
+import { Role } from '../model/role';
 
 @Component({
   selector: 'app-login',
@@ -16,15 +18,35 @@ export class LoginComponent {
   @ViewChild('loginForm') loginForm!: NgForm
   phoneNumber: string = '';
   password: string = '';
-  constructor(private router: Router, 
-    private userService: UserService,
-    private tokenService: TokenService
-  ) {
 
-  }
+  roles: Role[] = [];  // Mảng role
+  rememberMe: boolean = true;
+  selectedRole: Role | undefined;   // Biến lưu giá trị từ dropdown
 
   onPhoneNumberChange() {
     console.log(`Phone typed: ${this.phoneNumber}`);
+  }
+
+  constructor(private router: Router, 
+    private userService: UserService,
+    private tokenService: TokenService,
+    private roleService: RoleService
+  ) { }
+
+  ngOnInit() {
+    // Gọi API lấy danh sách roles và lưu vào biến roles
+    debugger
+    this.roleService.getRoles().subscribe({
+      next: (roles: Role[]) => {
+        debugger
+        this.roles = roles;
+        this.selectedRole = roles.length > 0 ? roles[0] : undefined;
+      },
+      error: (error: any) => {
+        debugger
+        console.error('Error getting roles: ', error);
+      }
+    });
   }
 
   login() {
--- shopapp-angular/src/app/service/user.service.ts
+++ shopapp-angular/src/app/service/user.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { RegisterDTO } from '../dto/user/register.dto';
 import { LoginDTO } from '../dto/user/login.dto';
 import { environment } from '../environment/environment';
+import { HttpUtilService } from './http.util.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,22 +12,20 @@ import { environment } from '../environment/environment';
 export class UserService {
   private apiRegister = `${environment.apiBaseUrl}/users/register`;
   private apiLogin = `${environment.apiBaseUrl}/users/login`;
-  private apiConfig = {
-    headers: this.createHeaders(),
+
+  constructor(
+    private http: HttpClient,
+    private httpUtilService: HttpUtilService
+  ) {}
+
+  register(registerDTO: RegisterDTO): Observable<any> {
+    const apiConfig = { headers: this.httpUtilService.createHeaders() };
+    return this.http.post(this.apiRegister, registerDTO, apiConfig);
   }
-  constructor(private http: HttpClient) { }
-    private createHeaders(): HttpHeaders {
-      return new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Accept-Language': 'vi'
-      })
-    }
 
-    register(registerDTO: RegisterDTO): Observable<any> {
-      return this.http.post(this.apiRegister, registerDTO, this.apiConfig)
-    }
-
-    login(loginDTO: LoginDTO): Observable<any> {
-      return this.http.post(this.apiLogin, loginDTO, this.apiConfig)
-    }
+  login(loginDTO: LoginDTO): Observable<any> {
+    const apiConfig = { headers: this.httpUtilService.createHeaders() };
+    return this.http.post(this.apiLogin, loginDTO, apiConfig);
+  }
 }
+
